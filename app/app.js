e1ed5be9a332c2fe99b67d96df9eb10f3fd618c6
sosloahn <sosloahn@daum.net>
2023-12-29 17:04:22 +0900

    파일 추가 분리

--- app/app.js
+++ app/app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 
-const home = require('./routes/home');
+const home = require('./src/routes/home');
 app.use('/', home); // use -> 미들웨어를 등록해주는 메소드
 
 
