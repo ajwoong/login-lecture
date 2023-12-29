d6796a71414cc474599000d2e4119d8b6fe95b0d
sosloahn <sosloahn@daum.net>
2023-12-29 15:58:56 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+const home = require('./routes/home');
+app.use('/', home); // use -> 미들웨어를 등록해주는 메소드
+
+
+module.exports = app;
