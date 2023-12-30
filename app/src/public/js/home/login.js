c790d7958720773dd16474afb81da2afd60c3614
sosloahn <sosloahn@daum.net>
2023-12-30 20:56:15 +0900

    로그인 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,13 +7,19 @@ const loginBtn = document.querySelector('button');
 loginBtn.addEventListener('click', login);
 
 
-
 function login(){
     const req = {
         id : id.value,
         psword: psword.value,
     };
-    console.log(req);
+
+    fetch('/login', {
+    method: 'POST',
+    headers:{
+        'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(req),
+    })
 };
 
 
